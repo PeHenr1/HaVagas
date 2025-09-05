29b1e1ad55a970fd0e05c3885f507320c8a7bb81
PeHenr1 <phenriqueaissa@gmail.com>
2025-09-05 01:25:41 -0300

    feat: mostrar formulario

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039048/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039048/havagas/MainActivity.kt
@@ -93,6 +93,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun mostrarFormulario() {
+        val nome = findViewById<EditText>(R.id.editTextNome).text.toString()
+        val email = findViewById<EditText>(R.id.editTextEmail).text.toString()
+        val telefone = findViewById<EditText>(R.id.editTextTelefone).text.toString()
+        val celular = editTextCelular.text.toString()
+        val dataNascimento = findViewById<EditText>(R.id.editTextDataNascimento).text.toString()
+        val formacao = spinnerFormacao.selectedItem.toString()
+        val ano = editTextAno.text.toString()
+        val instituicao = editTextInstituicao.text.toString()
+        val titulo = editTextTitulo.text.toString()
+        val orientador = editTextOrientador.text.toString()
+        val sexo = spinnerSexo.selectedItem.toString()
+        val sexoOutro = editTextSexoOutro.text.toString()
+        val vagas = findViewById<EditText>(R.id.editTextVagasInteresse).text.toString()
+
+        val mensagem = StringBuilder()
+        mensagem.append("Nome: $nome\n")
+        mensagem.append("E-mail: $email\n")
+        mensagem.append("Telefone: $telefone\n")
+        if (checkBoxCelular.isChecked) mensagem.append("Celular: $celular\n")
+        mensagem.append("Data de Nascimento: $dataNascimento\n")
+        mensagem.append("Sexo: ${if (sexo == "Outro") sexoOutro else sexo}\n")
+        mensagem.append("Formação Acadêmica: $formacao\n")
+        if (editTextAno.visibility == View.VISIBLE) mensagem.append("Ano de Conclusão: $ano\n")
+        if (editTextInstituicao.visibility == View.VISIBLE) mensagem.append("Instituição: $instituicao\n")
+        if (editTextTitulo.visibility == View.VISIBLE) mensagem.append("Título Monografia: $titulo\n")
+        if (editTextOrientador.visibility == View.VISIBLE) mensagem.append("Orientador: $orientador\n")
+        mensagem.append("Vagas de Interesse: $vagas\n")
+
+        AlertDialog.Builder(this)
+            .setTitle("Formulário Preenchido")
+            .setMessage(mensagem.toString())
+            .setPositiveButton("OK", null)
+            .show()
+    }
+
     private fun limparFormulario() {
         val editTexts = listOf<EditText>(
             findViewById(R.id.editTextNome),
